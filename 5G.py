d99e3dfe51347649f20b69de896b02fba841e81f
Igor1208gabriel <131968406+Igor1208gabriel@users.noreply.github.com>
2023-05-02 07:30:25 -0300

    Update 5G.py

---
+++ 5G.py
@@ -0,0 +1,9 @@
+    a, b, c = map(int, input().split())
+     
+    if a-b == 0:print("S")
+    elif a-c == 0:print("S")
+    elif b-c == 0:print("S")
+    elif a - (b+c) == 0:print("S")
+    elif b - (a+c) == 0:print("S")
+    elif c - (a+b) == 0:print("S")
+    else:print("N")
