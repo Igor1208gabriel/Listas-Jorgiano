be7218a52e0f1b873be3c4d89f7ca0f80992dce5
Igor1208gabriel <131968406+Igor1208gabriel@users.noreply.github.com>
2023-05-03 13:33:31 -0300

    Update 5E.py

--- 5E.py
+++ 5E.py
@@ -1,12 +1,20 @@
-consumo = int(input())
+def calcular_valor_conta_agua(consumo):
+    valor_total = 7  # Valor da assinatura básica
 
-preco = 7
-if consumo > 10 and consumo <= 30:
-        preco += (consumo-10)*1
-elif consumo > 30 and consumo <= 100:
-        preco += (consumo-20)*2
-elif consumo > 101:
-        preco += (consumo-30)+((consumo-100)*2)*5
+    if consumo > 10:
+        if consumo <= 30:
+            valor_total += (consumo - 10) * 1
+        elif consumo <= 100:
+            valor_total += 20  # Valor para o intervalo de 11 a 30m3
+            valor_total += (consumo - 30) * 2
+        else:
+            valor_total += 20  # Valor para o intervalo de 11 a 30m3
+            valor_total += 70 * 2  # Valor para o intervalo de 31 a 100m3
+            valor_total += (consumo - 100) * 5
+
+    return valor_total
 
 
-print(preco)
+consumo = int(input())  # Leitura do consumo de água da residência
+valor_conta = calcular_valor_conta_agua(consumo)
+print(valor_conta)
