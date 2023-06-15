d9bf87fb1d63f6905fbfa6a9fbe147375f52379b
Igor1208gabriel <131968406+Igor1208gabriel@users.noreply.github.com>
2023-06-15 09:59:52 -0300

    Add files via upload

---
+++ 7a.py
@@ -0,0 +1,5 @@
+a = int(input())
+
+for i in range(1, 11):
+    print(f"{i} x {a} = {i*a}")
+
---
+++ 7d.py
@@ -0,0 +1,6 @@
+a = int(input())
+lista=[]
+for i in range(1,a+1):
+    if a%i == 0:
+        lista.append(i)
+print(*lista)
